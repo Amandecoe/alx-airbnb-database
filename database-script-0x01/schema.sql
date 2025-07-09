fada1ad3bfcc8356fb5838c37d6a70655009a3ad
Amanuel <amanuelbehailu557@gmail.com>
2025-07-09 16:41:42 +0300

    tuser table created

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,2 +1,14 @@
 USE alxdb;
+DROP TABLE USER;
+CREATE TABLE USER(
+  user_id INT PRIMARY KEY,
+  UUID CHAR(36) UNIQUE,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100)NOT NULL UNIQUE, 
+  password_hash VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(100) NULL,
+  role_ ENUM('guest', 'host', 'admin') NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
