1b4481e8e63af6f324695463b8ba7cce32cc7e66
Amanuel <amanuelbehailu557@gmail.com>
2025-07-10 15:45:26 +0300

    Data added to User table

--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -1,1 +1,4 @@
+INSERT INTO USER
+(user_id, first_name, last_name, email, password_hash, phone_number, role_)
+VALUES (1, "Amanuel","Behailu", "Amanuelbehailu557@gmail.com", "aman123", 0967934788, 'guest'  )
 
