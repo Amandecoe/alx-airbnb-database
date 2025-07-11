a9c70dc40c331710dfa4c1f2c2fd9d4a8a627a23
Amanuel <amanuelbehailu557@gmail.com>
2025-07-11 14:16:12 +0300

    All info displayed

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,5 +2,4 @@ SELECT booking.booking_id, user.user_id FROM booking INNER JOIN user
 ON booking.booking_id = user.user_id;
 
 SELECT name_, description_, location_, pricepernight, rating,comment FROM property
-LEFT JOIN review ON property.property_id = review.property_id
-WHERE property.property_id = 1;
+LEFT JOIN review ON property.property_id = review.property_id;
