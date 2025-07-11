bbf095c44fafbe4e65e354d95a49ed53b54b58d1
Amanuel <amanuelbehailu557@gmail.com>
2025-07-11 16:38:03 +0300

    RANK

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,6 @@
+SELECT user.user_id, COUNT(booking.booking_id) AS booking_count 
+FROM USER LEFT JOIN booking ON user.user_id = booking.user_id
+GROUP BY user.user_id;
+
+SELECT user.user_id, COUNT(booking.booking_id) AS booking_count, ROW_NUMBER() over (ORDER BY booking_count DESC)
+FROM USER LEFT JOIN booking ON user.user_id = booking.user_id
