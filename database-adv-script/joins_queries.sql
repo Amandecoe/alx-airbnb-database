a73591d2f3c390648eec1cd39b668cececba02cf
Amanuel <amanuelbehailu557@gmail.com>
2025-07-11 14:14:57 +0300

    Left join applied

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,2 +1,6 @@
 SELECT booking.booking_id, user.user_id FROM booking INNER JOIN user
 ON booking.booking_id = user.user_id;
+
+SELECT name_, description_, location_, pricepernight, rating,comment FROM property
+LEFT JOIN review ON property.property_id = review.property_id
+WHERE property.property_id = 1;
