6a85221fc98b932f68abcd41c7264498ccc71dff
Amanuel <amanuelbehailu557@gmail.com>
2025-07-11 14:18:24 +0300

    ordered

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,4 +2,5 @@ SELECT booking.booking_id, user.user_id FROM booking INNER JOIN user
 ON booking.booking_id = user.user_id;
 
 SELECT name_, description_, location_, pricepernight, rating,comment FROM property
-LEFT JOIN review ON property.property_id = review.property_id;
+LEFT JOIN review ON property.property_id = review.property_id
+ORDER BY property.property_id, review.rating DESC;
