264473b6c2c3890b19c306290cee09c27c15791b
Amanuel <amanuelbehailu557@gmail.com>
2025-07-11 17:27:30 +0300

    Display every data

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,4 @@
-SELECT * FROM Booking RIGHT JOIN USER ON booking_id.user=user.user_id
+SELECT * FROM BOOKING LEFT JOIN USER ON BOOKING.user_id = USER.user_id
+LEFT JOIN PROPERTY ON BOOKING.property_id = PROPERTY.property_id
+WHERE USER.user_id = 1
+AND booking_id = 1;
